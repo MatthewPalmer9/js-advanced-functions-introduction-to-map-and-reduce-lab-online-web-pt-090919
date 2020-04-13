69bc628d01214fc1539864a23e25f5c7efc4764f
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-13 17:28:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function mapToNoChange(sourceArray){
 function mapToDouble(sourceArray){
   return sourceArray.map(x => x*2);
 }
+
+function mapToSquare(sourceArray){
+  return sourceArray.map(x => x^2);
+}

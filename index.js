321adcaf8b95c73809d56520dc9bd830758cbe12
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-04-13 17:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Your code here
 
 function mapToNegativise(arr){
-  let newArr = []
-  for(let i = 0; i < arr, i++){
+  let newArr = [];
+  for(let i = 0; i < arr; i++){
     newArr.push(arr[i*-1])
   }
   return newArr
